90d7f84f487014e95c325540e5a65c43fde81559
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2025-01-06 19:50:09 +0500

    Exam File Created

---
+++ SecondModul_Exam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SecondModul_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
